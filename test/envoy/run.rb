2adf8f86fca21056caae4a09a5f036a4c214d232
hiromi-ogawa <hiromi-ogawa@cookpad.com>
2023-05-17 16:41:28 +0900

    Run integration test with v3 API

--- test/envoy/run.rb
+++ test/envoy/run.rb
@@ -4,14 +4,13 @@ require 'yaml'
 require 'json'
 require 'resolv'
 require 'pp'
+require 'erb'
 $stdout.sync = true
 
-ip = Resolv.getaddress('statsd-exporter')
-p ip
+statsd_exporter_ip = Resolv.getaddress('statsd-exporter')
+p statsd_exporter_ip
 
-config = YAML.load_file('/config.yaml')
-config['stats_sinks'][0]['config']['address']['socket_address']['address'] = ip
+config = YAML.load(ERB.new(File.read('/config.yaml.erb')).result)
 File.open('/tmp/generated.json', 'w') { |f| f.puts(JSON.pretty_generate(config)) }
 
-exec('envoy', '-c', '/tmp/generated.json', '--bootstrap-version', '2', '--service-cluster', 'book', '--service-node', 'book',
-'--log-level', 'info')
+exec('envoy', '-c', '/tmp/generated.json', '--service-cluster', 'book', '--service-node', 'book', '--log-level', 'info')
